4e19adefe847e5a9f8ec255918b2743550040763
avinash8827 <avinashsinghbilawat279@gmail.com>
2021-11-22 19:01:48 +0530

    completed

--- index.php
+++ index.php
@@ -4,38 +4,77 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <title>PHP QueryString</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     </head>
     <body>
-        <?php
-        if (isset($_GET['name'])) {
-           // echo 'yes';
-            echo $_GET['name'];
-            echo $_GET['frisurname'];
-            echo $_GET['fricontact'];
-            echo $_GET['friaddr'];
-            $name = $_GET['name'];
-            $frisurename = $_GET['frisurname'];
-            $fricontact = $_GET['fricontact'];
-            $friaddr = $_GET['friaddr'];
+        <?php  
+           
 
-          $conn = mysqli_connect('localhost','root','','mydatabase_db')
-
-          $sql = "INSERT INTO friends(  friend_name,surname,contact,addr)VALUES('$name','$surename','$contact','$addr')";
+            if(isset($_GET['name'])){ 
+                
 
 
-          mysqli_query($conn,$sql);
+                echo $_GET['name'];
+                echo $_GET['course'];
 
-          mysqli_close($conn);
+                $name = $_GET['name'];
+                $course = $_GET['course'];
+                $contact = $_GET['contact'];
+
+                
+                $conn = mysqli_connect('localhost','root','','mydatabase_db');
+
+                $sql = "INSERT INTO students(student_nam,student_cours,student_contact)VALUES('$name','$course','$contact')";
 
 
-          echo 'Data Sucees';
-        }else {
-            echo 'no' ;
-            
-        }
-        
+
+                mysqli_query($conn,$sql);
+
+
+                echo 'Data Inserted Successfully';
+
+
+
+
+              
+
+                mysqli_close($conn);
+
+
+
+            }else{
+                
+                echo 'No Parameter';
+            }
+
+         
         ?>
-     
+              <form action="">
+            <fieldset>
+                <legend>Persanol information</legend>
+                <div>
+                    <label for="sn">student Name :</label>
+                    <input type="text" id="sn" name="name" required>
+                </div>
+                <div>
+                    <label for="sc">student coure :</label>
+                    <input type="text" id="sc" name="coure" required>
+                </div>
+                <div>
+                    <label for="scn">student contact:</label>
+                    <input type="text" id="scn" name="contact" required>
+                </div>
+
+            </fieldset>
+            <div >
+            <input type="submit" value="submit"class="bt">
+        </div>
+        </form>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <script>
+    
+        </script>
     </body>
 </html>
