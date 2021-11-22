8e74f81539995c59f93e1d9a253fb8958f3965d4
avinash8827 <avinashsinghbilawat279@gmail.com>
2021-11-22 17:31:22 +0530

    incomplete

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+        if (isset($_GET['name'])) {
+           // echo 'yes';
+            echo $_GET['name'];
+            echo $_GET['surname'];
+            echo $_GET['contact'];
+            echo $_GET['addr'];
+            $name = $_GET['name'];
+            $surename = $_GET['surname'];
+            $contact = $_GET['contact'];
+            $addr = $_GET['addr'];
+
+          $conn = mysqli_connect('localhost','root','','mydatabase_db')
+
+          $sql = "INSERT INTO friends(  friend_name,surname,contact,addr)VALUES('name','$surename','$contact','$addr')";
+
+
+          mysqli_query($conn,$sql);
+
+          mysqli_close($conn);
+
+
+          echo 'Data Sucees';
+        }else {
+            echo 'no' ;
+            
+        }
+        
+        ?>
+     
+    </body>
+</html>
